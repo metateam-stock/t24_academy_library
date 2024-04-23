8f1ee673e8df7676c914581de1c4228e20ab5c2c
kuwanaho <naho.kuwahara@metateam.co.jp>
2024-04-23 20:43:03 +0900

    貸出一覧、登録機能修正

--- src/main/java/jp/co/metateam/library/controller/RentalManageController.java
+++ src/main/java/jp/co/metateam/library/controller/RentalManageController.java
@@ -1,37 +1,53 @@
 package jp.co.metateam.library.controller;
+ 
+import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+ 
+import jakarta.validation.Valid;
+import jp.co.metateam.library.model.Stock;
+import jp.co.metateam.library.model.StockDto;
+import jp.co.metateam.library.service.StockService;
+import jp.co.metateam.library.model.Account;
+import jp.co.metateam.library.model.AccountDto;
 import jp.co.metateam.library.service.AccountService;
 import jp.co.metateam.library.service.RentalManageService;
-import jp.co.metateam.library.service.StockService;
 import lombok.extern.log4j.Log4j2;
-
+import jp.co.metateam.library.model.RentalManageDto;
+import jp.co.metateam.library.model.RentalManage;
+import jp.co.metateam.library.values.RentalStatus;
+ 
 /**
  * 貸出管理関連クラスß
  */
 @Log4j2
 @Controller
 public class RentalManageController {
-
+ 
     private final AccountService accountService;
     private final RentalManageService rentalManageService;
     private final StockService stockService;
-
+ 
     @Autowired
     public RentalManageController(
-        AccountService accountService, 
-        RentalManageService rentalManageService, 
+        AccountService accountService,
+        RentalManageService rentalManageService,
         StockService stockService
     ) {
         this.accountService = accountService;
         this.rentalManageService = rentalManageService;
         this.stockService = stockService;
     }
-
+ 
     /**
      * 貸出一覧画面初期表示
      * @param model
@@ -39,12 +55,48 @@ public class RentalManageController {
      */
     @GetMapping("/rental/index")
     public String index(Model model) {
-        // 貸出管理テーブルから全件取得
-
+        // 貸出テーブルから全件取得
+        List <RentalManage> rentalList = this.rentalManageService.findAll();
         // 貸出一覧画面に渡すデータをmodelに追加
-
+        model.addAttribute("rentalList", rentalList);
         // 貸出一覧画面に遷移
-        return "";
+        return "rental/index";
     }
+    @GetMapping("/rental/add")
+    public String add(Model model) {
+        List<RentalManage> rentalManageList = this.rentalManageService.findAll();
+        List<Account> accounts = this.accountService.findAll();
+        List<Stock> stockList = this.stockService.findAll();
+        model.addAttribute("rentalManageList", rentalManageList);
+        model.addAttribute("accounts", accounts);
+        model.addAttribute("stockList", stockList);
 
+        model.addAttribute("rentalStatus", RentalStatus.values());
+        if (!model.containsAttribute("rentalManageDto")) {
+            model.addAttribute("rentalManageDto", new RentalManageDto());
+        }
+ 
+        return "rental/add";
+    }
+    @PostMapping("/rental/add")
+    public String save(@Valid @ModelAttribute RentalManageDto rentalManageDto, BindingResult result, RedirectAttributes ra) {
+        try {
+            if (result.hasErrors()) {
+                throw new Exception("Validation error.");
+            }
+            // 登録処理
+            this.rentalManageService.save(rentalManageDto);
+
+            return "redirect:/rental/index";
+        } catch (Exception e) {
+            log.error(e.getMessage());
+
+            ra.addFlashAttribute("rentalManageDto", rentalManageDto);
+            ra.addFlashAttribute("org.springframework.validation.BindingResult.stockDto", result);
+
+           
+            return "redirect:/rental/add";
+        } 
+}
+  
 }
